76e762e7211fc5189b8973ce25760ab23769eebb
murmelbahn <tim@susa.pw>
2019-12-03 18:35:22 +0100

    Fixed spelling error

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 ## Install packages
+
 apt install -y zsh sshpass htop figlet tmux curl
 curl -L git.io/antigen > ~/.antigen.zsh
+cd ~
 
 ## Make ZSH as default Shell
 chsh -s $(which zsh)
 
 ## Remove existing config files
 if test -f "~/.zshrc"; then
-rm ~/.zshrc
+rm .zshrc
     fi
 if test -f "~/.antigen.zsh"; then
 rm ~/.antigen.zsh
@@ -15,9 +17,10 @@ rm ~/.antigen.zsh
 if test -f "~/.p10k.zsh"; then
 rm ~/.p10k.zsh
     fi
+
 ## Symlink for new config
-cd ~
 ln -s ~/.zshconfig/zshrc .zshrc 
+
 ## Emtpy Motd
 if test -f "/etc/update-motd.d/10-uname"; then
 rm /etc/update-motd.d/10-uname
@@ -29,17 +32,18 @@ if test -f "/etc/update-motd.d/10-sysinfo"; then
 rm /etc/update-motd.d/10-sysinfo
     fi
 if test -f "/etc/update-motd.d/90-footer"; then
-rm /etc/update-motd.d/90.footer
+rm /etc/update-motd.d/90-footer
     fi
+
 ## Copy Files, fix permissions
 cp ~/.zshconfig/motd/* /etc/update-motd.d/
 chmod +x /etc/update-motd.d/*
 echo > /etc/motd
+
 ## htop config
 if test -f "~/.config/htop/htoprc"; then
 rm ~/.config/htop/htoprc
     fi
-cd ~
 ln -s ~/.zshconfig/htoprc .config/htop/htoprc
 
 
