4eda095f06dee08797f65728431c1436f4fb5191
murmelbahn <tim@susa.pw>
2019-12-03 18:08:13 +0100

    Added tmux

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
 ## Install packages
-apt install -y zsh sshpass htop figlet curl
+apt install -y zsh sshpass htop figlet tmux curl
 curl -L git.io/antigen > ~/.antigen.zsh
 
 ## Make ZSH as default Shell
 chsh -s $(which zsh)
 
 ## Remove existing config files
-if test -f "$FILE"; then
+if test -f "~/.zshrc"; then
 rm ~/.zshrc
     fi
+if test -f "~/.antigen.zsh"; then
+rm ~/.antigen.zsh
+    fi
+if test -f "~/.p10k.zsh"; then
+rm ~/.p10k.zsh
+    fi
+
 
 ## Symlink for new config
 cd ~
