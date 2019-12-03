0269baea9cadcf991e20879b047425165f1d81e3
murmelbahn <tim@susa.pw>
2019-12-03 17:46:41 +0100

    Fixed figlet

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 ## Install packages
-apt install -y zsh zsh-antigen sshpass htop finglet curl
+apt install -y zsh zsh-antigen sshpass htop figlet curl
 
-
-## Set ZSH as default shell
+## Make ZSH as default Shell
 chsh -s $(which zsh)
 
-
+## Remove existing config files
+if test -f "$FILE"; then
 rm ~/.zshrc
+    fi
+
+## Symlink for new config
 cd ~
 ln -s ~/.zshconfig/zshrc .zshrc 
