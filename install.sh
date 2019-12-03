87b794d11fc7c7dcc013322be2dee4975ac0a91b
murmelbahn <tim@susa.pw>
2019-12-03 17:38:28 +0100

    Initial

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
-zsh
-zsh-antigen
-sshpass
-htop
-finglet
-curl
-git
+## Install packages
+apt-get install -y zsh zsh-antigen sshpass htop finglet curl git
 
+
+## Set ZSH as default shell
+chsh -s $(which zsh)
+
+
+rm ~/.zshrc
+cd ~
+ln -s ~/.zshconfig/zshrc .zshrc 
