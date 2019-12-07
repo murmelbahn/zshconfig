fe25c2a8904f1aa9f768d9acfa99ee8fe043f0ef
Tim Susa <tim@susa.pw>
2019-12-07 20:26:18 +0100

    Extend OSX support

--- install.sh
+++ install.sh
@@ -2,11 +2,25 @@
 
 ## Define Arrays
 packages=( "zsh" "htop" "figlet" "tmux" "curl" )
-configfiles=( ".zshrc" ".antigen.zsh" ".p10k.zsh" ".tmux.conf" ".config/htop/htoprc" "/etc/nanorc")
+configfiles=( ".zshrc" ".antigen.zsh" ".p10k.zsh" ".tmux.conf" ".config/htop/htoprc" )
+configfileslinux=( "/etc/nanorc" )
 motdfiles=( "/etc/update-motd.d/00-header" "/etc/update-motd.d/10-uname" "/etc/update-motd.d/10-sysinfo" "/etc/update-motd.d/90-footer" )
+
+
 cd ~
-mkdir .config/htop
+
+if test -d ~/.config/htop; then
+  echo Htop folder found - skipping removing.
+else
+mkdir ~/.config/htop
+fi
+
+
 ## Install packages
+
+if [ "$(uname)" = "Darwin" ]; then
+  echo MacOS found - no installation of packages needed.
+else
 for i in "${packages[@]}"
 do
 
@@ -19,41 +33,64 @@ echo $i is installed - skipping.
         apt install -y $i
         fi
  done
+fi
 
 ## Remove Config Files
 
 for j in "${configfiles[@]}"
 do
   rm $j
-done 
+done
+
+if [ "$(uname)" != "Darwin" ]; then
+  for l in "${configfileslinux[@]}"
+  do
+    rm $j
+  done
+fi
+
 
 ## Remove Motd Files
-
+if [ "$(uname)" = "Darwin" ]; then
+  echo MacOS found - no motd files to remove.
+else
 for k in "${motdfiles[@]}"
 do
   if test -f $k; then
   rm $k
   fi
 done
+fi
 
 ## Install Antigen
 curl -L git.io/antigen > .antigen.zsh
-
 
 ## Create Symlinks
 ln -s ~/.zshconfig/zshrc .zshrc
 ln -s ~/.zshconfig/tmux .tmux.conf
 ln -s ~/.zshconfig/htoprc .config/htop/htoprc
 ln -s ~/.zshconfig/p10k.zsh .p10k.zsh
-ln -s ~/.zshconfig/nanorc /etc/nanorc
+if [ "$(uname)" != "Darwin" ]; then
+  ln -s ~/.zshconfig/nanorc /etc/nanorc
+else
+  echo MacOS found - No linking of Nanorc needed.
+fi
+
+
 
 ## Make ZSH as default Shell
+if [ "$(uname)" = "Darwin" ]; then
+  echo MacOS found - ZSH is already default shell.
+else
 chsh -s $(which zsh)
-
+fi
 
 
 ## Copy Files, fix permissions
+if [ "$(uname)" = "Darwin" ]; then
+  echo MacOS found - No motd changes needed.
+else
 cp ~/.zshconfig/motd/* /etc/update-motd.d/
 chmod +x /etc/update-motd.d/*
 echo > /etc/motd
-
+fi
